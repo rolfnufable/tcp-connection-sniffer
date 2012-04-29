a0834a4a2bb279c9286477b026e88614776d04b8
beam.liu@gmail.com <beam.liu@gmail.com@68b9f4a0-b2b9-c869-1578-7ed89b6dace7>
2012-04-29 05:34:04 +0000

    add jpcap filter definition at java level, this filter should be done by jpcap native library, as the wincap\libcap doesn't support pppoe packets, so we need do the filter by Jpcap itself.

---
+++ jpcap/src/java/jpcap/JpcapFilter.java
@@ -0,0 +1,184 @@
+package jpcap;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * This class is used to do a very simple filtering setting for packets, all of
+ * those criteria are "and" operation, usually, if it is not a pppoe/vpn network
+ * environment, suggest to use {@link JpcapCaptor#setFilter(String, boolean)},
+ * which is much more powerful. This filter action is done at native level, so
+ * it will be a bit faster than doing it in Java after packet instances are
+ * created.
+ */
+public class JpcapFilter {
+	private List<String> hosts = new ArrayList<String>();
+	private List<String> srcHosts = new ArrayList<String>();
+	private List<String> destHosts = new ArrayList<String>();
+	private List<String> ports = new ArrayList<String>();
+	private List<String> srcPorts = new ArrayList<String>();
+	private List<String> destPorts = new ArrayList<String>();
+	private List<Protocol> protocols = new ArrayList<Protocol>();
+
+	/**
+	 * get the host ip in the filter, either src host or dest host
+	 * 
+	 * @return host ip list
+	 */
+	public List<String> getHosts() {
+		return hosts;
+	}
+
+	/**
+	 * Add a host filter, it should be a ip, either src host or dest host
+	 * matches the given host, then the packet will pass this filter
+	 * 
+	 * @param host
+	 *            src host ip or dest host ip
+	 */
+
+	public void addHost(String host) {
+		this.hosts.add(host);
+	}
+
+	/**
+	 * get the host port in the filter, either src port or dest port
+	 * 
+	 * @return host port list
+	 */
+	public List<String> getPorts() {
+		return ports;
+	}
+
+	/**
+	 * Add a port filter, either src port or dest port matches the given port,
+	 * then the packet will pass this filter
+	 * 
+	 * @param port
+	 *            src port or dest port
+	 */
+	public void addPort(int port) {
+		this.ports.add(port + "");
+	}
+
+	/**
+	 * get the protocols in the filter
+	 * 
+	 * @return protocols in String format in the filer
+	 */
+	public List<String> getProtocols() {
+		List<String> protocolStrs = new ArrayList<String>();
+		for (Protocol p : protocols) {
+			protocolStrs.add(p.name());
+		}
+		return protocolStrs;
+	}
+
+	/**
+	 * Add a protocol filter, currently supports TCP,UDP,ICMP,ARP
+	 * 
+	 * @param protocol
+	 *            protocol,should be either of TCP,UDP,ICMP,ARP
+	 * 
+	 * @see Protocol
+	 */
+	public void addProtocol(Protocol protocol) {
+		this.protocols.add(protocol);
+	}
+
+	/**
+	 * get the src host ip in the filter
+	 * 
+	 * @return src host ip list
+	 */
+	public List<String> getSrcHosts() {
+		return srcHosts;
+	}
+
+	/**
+	 * Add a host filter, it should be a src host ip
+	 * 
+	 * @param srcHost
+	 *            src host ip
+	 */
+	public void addSrcHost(String srcHost) {
+		this.srcHosts.add(srcHost);
+	}
+
+	/**
+	 * get the dest host ip in the filter
+	 * 
+	 * @return dest host ip list
+	 */
+	public List<String> getDestHosts() {
+		return destHosts;
+	}
+
+	/**
+	 * Add a host filter, it should be a dest host ip
+	 * 
+	 * @param destHost
+	 *            dest host ip
+	 */
+	public void addDestHost(String destHost) {
+		this.destHosts.add(destHost);
+	}
+
+	/**
+	 * get the src ports in the filter
+	 * 
+	 * @return src port list
+	 */
+	public List<String> getSrcPorts() {
+		return srcPorts;
+	}
+
+	/**
+	 * Add a port filter, it should be src port
+	 * 
+	 * @param srcPort
+	 *            src port
+	 */
+	public void addSrcPort(int srcPort) {
+		this.srcPorts.add(srcPort + "");
+	}
+
+	/**
+	 * get the dest ports in the filter
+	 * 
+	 * @return dest port list
+	 */
+	public List<String> getDestPorts() {
+		return destPorts;
+	}
+
+	/**
+	 * Add a port filter, it should be dest port
+	 * 
+	 * @param destPort
+	 *            dest port
+	 */
+	public void setDestPorts(int destPort) {
+		this.destPorts.add(destPort + "");
+	}
+
+	public enum Protocol {
+		/**
+		 * TCP packet
+		 */
+		TCP,
+		/**
+		 * UDP packet
+		 */
+		UDP,
+		/**
+		 * ICMP packet
+		 */
+		ICMP,
+		/**
+		 * ARP packet
+		 */
+		ARP
+	}
+
+}
